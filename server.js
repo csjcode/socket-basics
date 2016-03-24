2a86124e34ab4e69847bd6c42a5103a8f869dc29
Chris St. John <christophersstjohn@gmail.com>
2016-03-24 16:49:03 +0800

    Add disconnect and msg

--- server.js
+++ server.js
@@ -12,6 +12,19 @@ var clientInfo = {};
 io.on('connection',function (socket) {
    console.log('User connected via socket.io');
 
+   socket.on('disconnect',function () {
+      var userData = clientInfo[socket.id];
+      if(typeof userData !== 'undefined'){
+         socket.leave(userData.room);
+         io.to(userData.room).emit('message',{
+            name: 'System',
+            text: userData.name + ' has left!',
+            timestamp: moment().valueOf()
+         });
+         delete clientInfo[socket.id];
+      }
+   });
+
    socket.on('joinRoom', function (req) {
       clientInfo[socket.id] = req;
       socket.join(req.room);
@@ -24,7 +37,7 @@ io.on('connection',function (socket) {
 
    socket.on('message',function (message) {
       console.log('Message received ' + message.text);
-      
+
       message.timestamp = moment().valueOf();
       io.to(clientInfo[socket.id].room).emit('message', message);// message emits ot everybody
    });
